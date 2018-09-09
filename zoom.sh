7143e6bedc5ee22985614130e8afae29e27d6405
Major Péter <majorpeter29@gmail.com>
2018-09-09 11:44:05 +0200

    initial commit (zoom.sh)

---
+++ zoom.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo " | iconName=zoom-fit-best"
+echo "---"
+echo "Zoom in | bash='xrandr --output HDMI-1 --scale 0.8x0.8' terminal=false iconName=zoom-in"
+echo "Default | bash='xrandr --output HDMI-1 --scale 1x1' terminal=false iconName=zoom-original"
+echo "Zoom out | bash='xrandr --output HDMI-1 --scale 1.25x1.25' terminal=false iconName=zoom-out"
